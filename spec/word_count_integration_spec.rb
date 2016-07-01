d2d6378a4e51a44b168fb7026ff8b63af599aae7
Sam Park <sampark18@gmail.com>
2016-07-01 10:38:20 -0700

    down to one error the fix was unnamed method switcheroo in word count

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -3,7 +3,7 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('countin words' , {:type => :feature}) do
+describe('countin words', {:type => :feature}) do
   it('Generates the number of times multiple-inputted words were input') do
     visit('/')
     fill_in('words', :with => 'Sam I am. Sam eats green eggs and ham')
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,12 +3,12 @@ require('word_count')
 
 describe('String#word_count')  do
   it("Shows the original string inputted by the user")  do
-    expect(("Sam I am. Sam eats green eggs and ham.")).word_to_count(eq("Sam I am. Sam eats green eggs and ham."))
+    expect(("Sam I am. Sam eats green eggs and ham.")).to(eq("Sam I am. Sam eats green eggs and ham."))
   end
   it("Find words that were input more than once")  do
-    expect(("Sam I am. Sam eats green eggs and ham.")).word_to_count(eq(2))
+    expect("Sam I am. Sam eats green eggs and ham.".word_count("Sam")).to(eq(2))
   end
-  # it('Find words that were input more than once, case insensitive') do
-  #   expect("Sam I am. Sam eats green eggs and Sam.".word_to_count("test")).to(eq(3))
-  # end
 end
+# it('Find words that were input more than once, case insensitive') do
+#   expect("Sam I am. Sam eats green eggs and Sam.".word_to_count("test")).to(eq(3))
+# end
